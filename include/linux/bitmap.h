cf92ad561d5fb0c9e09075faf3f362d9bb95f1cd
Randy Dunlap <rdunlap@infradead.org>
2017-10-16 16:32:51 -0700

    documentation: kernel-api: add more info on bitmap functions
    There are some good comments about bitmap operations in lib/bitmap.c
    and include/linux/bitmap.h, so format them for document generation and
    pull them into core-api/kernel-api.rst.
    
    I converted the "tables" of functions from using tabs to using spaces
    so that they are more readable in the source file and in the generated
    output.
    
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Jonathan Corbet <corbet@lwn.net>
    Signed-off-by: Panchajanya Sarkar <panchajanya@azure-dev.live>
    (cherry picked from commit caefe33bbd2c409c3821b572203b91236f5df247)

--- include/linux/bitmap.h
+++ include/linux/bitmap.h
@@ -64,14 +64,9 @@
  *  bitmap_find_free_region(bitmap, bits, order)  Find and allocate bit region
  *  bitmap_release_region(bitmap, pos, order)   Free specified bit region
  *  bitmap_allocate_region(bitmap, pos, order)  Allocate specified bit region
- *  bitmap_from_arr32(dst, buf, nbits)          Copy nbits from u32[] buf to dst
- *  bitmap_to_arr32(buf, src, nbits)            Copy nbits from buf to u32[] dst
+ *  bitmap_from_u32array(dst, nbits, buf, nwords)  *dst = *buf (nwords 32b words)
+ *  bitmap_to_u32array(buf, nwords, src, nbits) *buf = *dst (nwords 32b words)
  *
- * Note, bitmap_zero() and bitmap_fill() operate over the region of
- * unsigned longs, that is, bits behind bitmap till the unsigned long
- * boundary will be zeroed or filled as well. Consider to use
- * bitmap_clear() or bitmap_set() to make explicit zeroing or filling
- * respectively.
  */
 
 /**
@@ -88,12 +83,8 @@
  *  test_and_change_bit(bit, addr)      Change bit and return old value
  *  find_first_zero_bit(addr, nbits)    Position first zero bit in *addr
  *  find_first_bit(addr, nbits)         Position first set bit in *addr
- *  find_next_zero_bit(addr, nbits, bit)
- *                                      Position next zero bit in *addr >= bit
+ *  find_next_zero_bit(addr, nbits, bit)  Position next zero bit in *addr >= bit
  *  find_next_bit(addr, nbits, bit)     Position next set bit in *addr >= bit
- *  find_next_and_bit(addr1, addr2, nbits, bit)
- *                                      Same as find_next_bit, but in
- *                                      (*addr1 & *addr2)
  *
  */
 
