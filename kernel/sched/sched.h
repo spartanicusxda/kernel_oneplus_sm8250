1ad32f8aa158e997445e6f7a84bff71ea81c944e
Patrick Bellasi <patrick.bellasi@arm.com>
2019-04-04 10:24:43 +0100

    ANDROID: sched/tune: Move SchedTune cpu API into UtilClamp wrappers
    The a SchedTune CPU boosting API is currently used from sugov_get_util()
    to get the boosted utilization and to pass it into schedutil_freq_util().
    When UtilClamp is in use instead we call schedutil_freq_util() by
    passing in just the aggregated CFS+RT utilization and the clamping is
    done internally only on FREQUENCY_UTIL calls.
    
    This asymmetry is not required moreover, schedutil code is polluted by
    non-mainline SchedTune code.
    
    Wrap SchedTune API call related to cpu utilization boosting with a more
    generic and mainlinish UtilClamp call:
    
     - uclamp_util(cpu, util)  <= boosted_cpu_util(cpu)
    
    This new API is already used in schedutil_freq_util() to clamp the
    aggregated RT+CFS utilization on FREQUENCY_UTIL calls.
    
    Move the cpu boosting into uclamp_util so that we remove any SchedTune
    specific bit from kernel/sched/cpufreq_schedutil.c.
    
    Get rid of the no more required boosted_cpu_util(cpu) method and replace
    it with a stune_util(cpu, util) which signature is better aligned with
    its uclamp_util(cpu, util) wrapping method.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    [
     Since we are at that, fix compilation warning due to capacity_orig_of()
     being "defined by not used" generated by rt and deadline compilation
     units.
    ]
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2586,6 +2586,10 @@ unsigned long uclamp_rq_util_with(struct rq *rq, unsigned long util,
 	return clamp(util, min_util, max_util);
 }
 #else /* CONFIG_UCLAMP_TASK */
+
+unsigned long stune_util(int cpu, unsigned long other_util,
+				struct sched_walt_cpu_load *walt_load);
+
 static inline
 unsigned long uclamp_rq_util_with(struct rq *rq, unsigned long util,
 				  struct task_struct *p)
@@ -2605,6 +2609,13 @@ bool uclamp_boosted(struct task_struct *p);
 # endif
 #else
 # define arch_scale_freq_invariant()	false
+#endif
+
+#ifdef CONFIG_SMP
+static unsigned long __maybe_unused capacity_orig_of(int cpu)
+{
+	return cpu_rq(cpu)->cpu_capacity_orig;
+}
 #endif
 
 /**
@@ -2657,13 +2668,22 @@ static inline unsigned long cpu_util_rt(struct rq *rq)
 	return READ_ONCE(rq->avg_rt.util_avg);
 }
 
+static inline unsigned long cpu_util_freq(int cpu) 
+{
+	struct rq *rq = cpu_rq(cpu);
+
+	return min(cpu_util_cfs(rq) + cpu_util_rt(rq), capacity_orig_of(cpu));
+}
+
 #else /* CONFIG_CPU_FREQ_GOV_SCHEDUTIL */
+
 static inline unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 				 unsigned long max, enum schedutil_type type,
 				 struct task_struct *p)
 {
 	return 0;
 }
+
 #endif /* CONFIG_CPU_FREQ_GOV_SCHEDUTIL */
 
 #ifdef CONFIG_HAVE_SCHED_AVG_IRQ
