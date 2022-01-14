71913d5d88e3ef6716a25309567d7d8719e45191
FraSharp <s23265@iisve.it>
2022-01-14 18:57:06 +0100

    build script: update paths

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 export ARCH=arm64
 export CROSS_COMPILE_COMPAT="/home/fra/clang/bin/arm-linux-gnueabi-"
 
-PATH="/home/fra/liclang/bin:/home/fra/gcc-12/bin:${PATH}" \
+PATH="/home/francesca/clang/bin:/home/francesca/gcc-12/bin:${PATH}" \
 	make CROSS_COMPILE=aarch64-linux-gnu- ARCH=arm64 O=out LLVM_IAS=1 LLVM=1 vendor/kona-full_defconfig -j69
 
-PATH="/home/fra/liclang/bin:/home/fra/gcc-12/bin:${PATH}" \
+PATH="/home/francesca/clang/bin:/home/francesca/gcc-12/bin:${PATH}" \
 	make CROSS_COMPILE=aarch64-linux-gnu- ARCH=arm64 O=out LLVM_IAS=1 LLVM=1 -j69
