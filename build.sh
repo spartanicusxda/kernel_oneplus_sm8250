b01aa10a97b55c3c308ca0eb14f9f443bae9440c
FraSharp <s23265@iisve.it>
2022-01-20 00:30:07 +0100

    build script: refactor and use /home/francesca

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 export ARCH=arm64
-export CROSS_COMPILE_COMPAT="/home/francesca/proton-clang/bin/arm-linux-gnueabi-"
+export CROSS_COMPILE_COMPAT="$HOME/proton-clang/bin/arm-linux-gnueabi-"
 
-PATH="/home/francesca/clang/bin:/home/francesca/gcc-12/bin:${PATH}" \
-	make CROSS_COMPILE=aarch64-linux-gnu- ARCH=arm64 O=out LLVM_IAS=1 LLVM=1 vendor/kona-full_defconfig -j69
+PATH="$HOME/clang/bin:$HOME/gcc-12/bin:${PATH}" \
+	make O=out ARCH=arm64 \
+	CROSS_COMPILE=aarch64-linux-gnu- \
+	LLVM_IAS=1 LLVM=1 \
+	vendor/kona-full_defconfig \
+	-j69
 
-PATH="/home/francesca/clang/bin:/home/francesca/gcc-12/bin:${PATH}" \
-	make CROSS_COMPILE=aarch64-linux-gnu- ARCH=arm64 O=out LLVM_IAS=1 LLVM=1 -j69
+PATH="$HOME/clang/bin:$HOME/gcc-12/bin:${PATH}" \
+	make O=out ARCH=arm64 \
+	CROSS_COMPILE=aarch64-linux-gnu- \
+	LLVM_IAS=1 LLVM=1 \
+	-j69
