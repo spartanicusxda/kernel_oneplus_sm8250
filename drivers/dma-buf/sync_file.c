e106f098cbf5c722fbf50fff9a3621802fb5a00e
FraSharp <s23265@iisve.it>
2022-01-23 16:24:23 +0100

    Revert "Fixup: dma-buf/sync_file: Remove debug names from sync data structures"
    This reverts commit b770a18f9ec3d2077c0533a84d25dc710374ef69.

--- drivers/dma-buf/sync_file.c
+++ drivers/dma-buf/sync_file.c
@@ -380,9 +380,12 @@ static long sync_file_ioctl_fence_info(struct sync_file *sync_file,
 				       unsigned long arg)
 {
 	struct sync_file_info info;
+	struct sync_fence_info *fence_info = NULL;
 	struct dma_fence **fences;
-	size_t len, offset;
-	int num_fences, i;
+	__u32 size;
+	int num_fences, ret, i;
+	size_t offset;
+	size_t len;
 
 	arg += offsetof(typeof(info), status);
 	len = sizeof(info) - offsetof(typeof(info), status);
@@ -410,31 +413,37 @@ static long sync_file_ioctl_fence_info(struct sync_file *sync_file,
 	if (info.num_fences < num_fences)
 		return -EINVAL;
 
-	offset = offsetof(struct sync_fence_info, status);
+	offset = offsetof(typeof(*fence_info), status);
+	size = sizeof(*fence_info) - offset;
+	fence_info = kzalloc(size * num_fences, GFP_KERNEL);
+	if (!fence_info)
+		return -ENOMEM;
+
 	for (i = 0; i < num_fences; i++) {
-		struct {
-			__s32	status;
-			__u32	flags;
-			__u64	timestamp_ns;
-		} fence_info;
-		struct sync_fence_info *finfo = (void *)&fence_info - offset;
-		int status = sync_fill_fence_info(fences[i], finfo);
+		struct sync_fence_info *fence = (void *)&fence_info[i] - offset;
+		int status = sync_fill_fence_info(fences[i], fence);
 		u64 dest;
 
-		/* Don't leak kernel memory to userspace via finfo->flags */
-		finfo->flags = 0;
 		info.status = info.status <= 0 ? info.status : status;
-		dest = info.sync_fence_info + i * sizeof(*finfo) + offset;
-		if (copy_to_user(u64_to_user_ptr(dest), &fence_info,
-				 sizeof(fence_info)))
-			return -EFAULT;
+		dest = info.sync_fence_info + i * sizeof(*fence_info) + offset;
+		if (copy_to_user(u64_to_user_ptr(dest), &fence->status, size)) {
+			ret = -EFAULT;
+			goto out;
+		}
 	}
 
 no_fences:
 	info.num_fences = num_fences;
+
 	if (copy_to_user((void __user *)arg, &info.status, len))
-		return -EFAULT;
-	return 0;
+		ret = -EFAULT;
+	else
+		ret = 0;
+
+out:
+	kfree(fence_info);
+
+	return ret;
 }
 
 static long sync_file_ioctl(struct file *file, unsigned int cmd,
