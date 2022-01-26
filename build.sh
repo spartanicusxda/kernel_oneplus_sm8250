017c29a7f1eb86e9554f8be4f87c2ba848407572
FraSharp <s23265@iisve.it>
2022-01-26 21:01:33 +0100

    build script: remove useless ARCH=arm64
    Signed-off-by: FraSharp <s23265@iisve.it>

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@ export ARCH=arm64
 export CROSS_COMPILE_COMPAT="$HOME/proton-clang/bin/arm-linux-gnueabi-"
 
 PATH="$HOME/clang/bin:$HOME/gcc-12/bin:${PATH}" \
-	make O=out ARCH=arm64 \
+	make O=out \
 	CROSS_COMPILE=aarch64-linux-gnu- \
 	LLVM_IAS=1 LLVM=1 \
 	vendor/kona-full_defconfig \
 	-j69
 
 PATH="$HOME/clang/bin:$HOME/gcc-12/bin:${PATH}" \
-	time make O=out ARCH=arm64 \
+	time make O=out \
 	CROSS_COMPILE=aarch64-linux-gnu- \
 	LLVM_IAS=1 LLVM=1 \
 	-j69
