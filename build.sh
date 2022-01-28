805809c728163e29ab5d162bb6568a89034ac4a3
FraSharp <s23265@iisve.it>
2022-01-28 22:13:10 +0100

    build script: use latest linaro gcc
    Signed-off-by: FraSharp <s23265@iisve.it>

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 export ARCH=arm64
 export CROSS_COMPILE_COMPAT="$HOME/proton-clang/bin/arm-linux-gnueabi-"
 
-PATH="$HOME/clang/bin:$HOME/gcc-12/bin:${PATH}" \
+PATH="$HOME/clang/bin:$HOME/gcc/bin:${PATH}" \
 	make O=out \
 	CROSS_COMPILE=aarch64-linux-gnu- \
 	LLVM_IAS=1 LLVM=1 \
 	vendor/kona-full_defconfig \
 	-j69
 
-PATH="$HOME/clang/bin:$HOME/gcc-12/bin:${PATH}" \
+PATH="$HOME/clang/bin:$HOME/gcc/bin:${PATH}" \
 	time make O=out \
 	CROSS_COMPILE=aarch64-linux-gnu- \
 	LLVM_IAS=1 LLVM=1 \
