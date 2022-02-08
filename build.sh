adc66f95f0ce81006942e99ac4ec6f4598bd1c88
FraSharp <s23265@iisve.it>
2022-02-08 02:04:31 +0100

    build script: add DTC_EXT and refactor for defconfig
    Signed-off-by: FraSharp <s23265@iisve.it>

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 export ARCH=arm64
 export CROSS_COMPILE_COMPAT="$HOME/proton-clang/bin/arm-linux-gnueabi-"
+export DTC_EXT=$HOME/dtc
+
+if [[ -f "arch/arm64/configs/$1" || -f "arch/arm64/configs/vendor/$1" ]] ; then
+defconfig = $1
+else
+defconfig = shamika_defconfig
+fi
+
 
 PATH="$HOME/clang/bin:$HOME/gcc/bin:${PATH}" \
 	make O=out \
 	CROSS_COMPILE=aarch64-linux-gnu- \
 	LLVM_IAS=1 LLVM=1 \
-	vendor/kona-full_defconfig \
+	$defconfig \
 	-j69
 
 PATH="$HOME/clang/bin:$HOME/gcc/bin:${PATH}" \
