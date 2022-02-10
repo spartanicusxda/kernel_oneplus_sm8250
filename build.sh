34cda86c4e586848fba71529a1588156fa0755a1
FraSharp <s23265@iisve.it>
2022-02-10 15:41:48 +0100

    import build script
    Signed-off-by: FraSharp <s23265@iisve.it>

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+export ARCH=arm64
+export DTC_EXT=$HOME/dtc
+
+if [[ -f "arch/arm64/configs/$1" || -f "arch/arm64/configs/vendor/$1" ]] ; then
+defconfig=$1
+else
+defconfig=shamika_defconfig
+fi
+
+
+PATH="$HOME/clang/bin:$HOME/gcc/bin:${PATH}" \
+	make O=out \
+	CROSS_COMPILE=aarch64-linux-gnu- \
+	LLVM_IAS=1 LLVM=1 \
+	$defconfig \
+	-j69
+
+PATH="$HOME/clang/bin:$HOME/gcc/bin:${PATH}" \
+	time make O=out \
+	CROSS_COMPILE=aarch64-linux-gnu- \
+	LLVM_IAS=1 LLVM=1 \
+	-j69
